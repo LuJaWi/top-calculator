1b53d28644d4b78a331ec1dd84e4bf0aff54fa40
Luke Witt <Witt.Luke@gmail.com>
2022-04-16 09:51:02 -0400

    Outlining functions with some pseudocode

---
+++ script.js
@@ -0,0 +1,13 @@
+// Create a script that writes each number key press to the first index in an array
+
+// Multiple number key presses should concatenate that key with previous entries to build larger numbers
+
+// When an operation key is pressed, 
+// iterate to the next index position and save the operation type
+
+// When "=" is pressed, perform the operation on the two sets of numbers, display the results
+// and clear the holder array
+
+// If a number, an operator, and a number are followed by another operator, write the output
+// of the first operation set to the display and write the
+// output to the first array position and repeat the previous steps until "=" is pressed.
